ed61aa1a3bc233eca45c9e326d107e48ccb33661
Andrew Dixon <andrew.dixon@capgemini.com>
2021-11-30 01:23:10 +0000

    datamodels: build strava oauth url method

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,3 +1,4 @@
+import { getQueryPredicate } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
 import { StravaService } from '../../core/strava-service/strava.service';
 
@@ -13,22 +14,35 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
-  redirect_uri = 'http://localhost:4200/exchange_token'
-  urlscope = 'read,activity:read_all,activity:read'
 
-  // strava oauth (so) urls
-  so_base_url = 'http://www.strava.com/oauth/'
-  so_route = 'authorize'
-  so_client_id = `client_id=${this.stravaService.clientid}str`;
-  so_response_type = 'response_type=code';
-  so_redirect_url = `redirect_uri=${this.redirect_uri}`;
-  so_approval_prompt = 'approval_prompt=forced';
-  so_scope = `scope=${this.urlscope}`;
-  strava_oauth_url = this.so_base_url + this.so_route + '?' + this.so_client_id + '&' + this.so_response_type + '&' + this.so_redirect_url + '&' + this.so_approval_prompt + '&' + this.so_scope
+  StravaOAuth() {
+    let oauth_url = this.BuildStravaOAuthUrl(window.location.href)
+    window.location.href = oauth_url;
+  }
 
-  StravaOAuth(){
-    window.location.href = this.strava_oauth_url;
+  BuildStravaOAuthUrl(redirect_base_uri: string): string {
+
+    const url_sep = [
+      'https:/',
+      'www.strava.com',
+      'oauth'
+    ];
+
+    let qp = ['authorize'];
+    const params_dict = {
+      client_id: 49912,
+      response_type: 'code',
+      redirect_uri: ['http:/', 'localhost:4200', 'exchange_token'].join('/'),
+      approval_prompt: 'force',
+      scope: ['read', 'activity:read_all', 'activity:read'].join(',')
+    };
+
+    qp.push(Object.keys(params_dict).map(o=>o + '=' + params_dict[o]).join('&'))
+
+    url_sep.push(qp.join('?'))
+
+    return url_sep.join('/');
   }
 
 }
+
