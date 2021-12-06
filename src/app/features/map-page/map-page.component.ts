8f254e7d718361911cab1950a164e7c0594388f0
Andrew Dixon <andrew.dixon@capgemini.com>
2021-12-06 15:10:50 +0000

    plotlines: initial attempt at plotting unencrypted hardcoded coordinates

--- src/app/features/map-page/map-page.component.ts
+++ src/app/features/map-page/map-page.component.ts
@@ -14,7 +14,8 @@ export class MapPageComponent implements OnInit {
 
   @ViewChild('map', { static: true }) mapElement: any;
   map: google.maps.Map;
-
+  bounds: google.maps.LatLngBounds;
+  
   constructor(private stravaService: StravaService, private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation.extras.state as {
@@ -25,10 +26,12 @@ export class MapPageComponent implements OnInit {
   }
 
   access_token:string;
+  gm_center_lat = 51.507570;
+  gm_center_lng = -0.127811;
 
   ngOnInit(): void {
     const mapProperties = {
-      center: new google.maps.LatLng(51.507570, -0.127811),
+      center: new google.maps.LatLng(this.gm_center_lat,this.gm_center_lng),
       zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -46,7 +49,35 @@ export class MapPageComponent implements OnInit {
   }
 
   ShowResults(){
-    this.print = this.activities[0].name
-    console.log(this.activities)
+    this.print = this.activities[0].name;
+    console.log(this.print)
+  }
+
+  PlotResults(){
+    console.log("PlotResults");
+
+    var polyline = this.activities[0].map.summary_polyline;
+    console.log(polyline);
+    const polyline_decrypted = google.maps.geometry.encoding.decodePath(polyline);
+    console.log(polyline_decrypted);
+
+    const flightPlanCoordinates = [
+      { lat: 51.507570, lng: -0.137811 },
+      { lat: 51.607570, lng: -0.147811 },
+      { lat: 51.707570, lng: -0.157811 },
+      { lat: 51.587570, lng: -0.167811 },
+      { lat: 51.407570, lng: -0.177811 },
+      { lat: 51.557570, lng: -0.187811 },
+      { lat: 51.527570, lng: -0.1987811 }
+    ];
+    const flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+  
+    flightPath.setMap(this.map);
   }
 }
