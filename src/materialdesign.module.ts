5ae8dec9ace515ea2bf4207d1555615c1895056c
Andrew Dixon <andrew.dixon@capgemini.com>
2021-12-28 21:08:14 +0000

    RER-23: Add material design components

--- src/materialdesign.module.ts
+++ src/materialdesign.module.ts
@@ -3,13 +3,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
     exports: [
         MatButtonModule,
         MatDialogModule,
         MatProgressSpinnerModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule
     ]
 })
 
