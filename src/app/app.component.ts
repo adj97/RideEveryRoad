9e71ef0413468f377c3e89ba1203bb2a002fd071
Andrew Dixon <andrew.dixon@capgemini.com>
2021-12-28 11:38:54 +0000

    RER-23: Initial feedback dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { FeedbackDialogComponent } from './features/dialogs/feedback-dialog/feedback-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,13 @@ export class AppComponent{
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
-    // const dialogRef = this.dialog.open(FeedbackDialog, {
-    //   width: '250px'
-    // });
+    const dialogRef = this.dialog.open(FeedbackDialogComponent, {
+      width: '250px'
+    });
 
-    // dialogRef.afterClosed().subscribe(result => {
-    //   console.log('The dialog was closed');
-    // });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ExchangetokenPageComponent } from './features/exchangetoken-page/exchan
 import { DialogComponent } from './features/dialog/dialog.component';
 
 import {MaterialDesignModule} from '../materialdesign.module';
+import { FeedbackDialogComponent } from './feedback-dialog/feedback-dialog.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import {MaterialDesignModule} from '../materialdesign.module';
     LandingPageComponent,
     MapPageComponent,
     ExchangetokenPageComponent,
-    DialogComponent
+    DialogComponent,
+    FeedbackDialogComponent
   ],
   imports: [
     BrowserModule,
