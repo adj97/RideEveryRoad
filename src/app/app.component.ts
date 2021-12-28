929b460d1a83a54b42e3ae0b95116837a48be880
Andrew Dixon <andrew.dixon@capgemini.com>
2021-12-28 11:36:10 +0000

    RER-23: Blank feedback button, awaiting dialog dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,16 @@ export class AppComponent{
 
   title = 'Ride Every Road';
 
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    // const dialogRef = this.dialog.open(FeedbackDialog, {
+    //   width: '250px'
+    // });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    // });
   }
 
 }
