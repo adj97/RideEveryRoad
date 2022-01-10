2999e2dcbd5b5ccb11d813750313899b66764ddb
Andrew Dixon <andrew.dixon@capgemini.com>
2022-01-10 21:34:32 +0000

    RER-23: Change radio buttons to toggle buttons

--- src/materialdesign.module.ts
+++ src/materialdesign.module.ts
@@ -5,7 +5,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
     exports: [
@@ -15,7 +15,7 @@ import { MatRadioModule } from '@angular/material/radio';
         MatFormFieldModule,
         MatInputModule,
         MatSnackBarModule,
-        MatRadioModule
+        MatButtonToggleModule
     ]
 })
 
