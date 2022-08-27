4271b7bd6a9feb5a3b26d222989a4a9c15fda701
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-08-27 12:46:34 +0100

    Messy but working cached auth
     - need to keep all strava auth objects in the service, not locally with
    each component
     - next is to cache previously loaded activities (probably as their
    encrypted polyline)

--- src/app/core/strava-service/strava.service.ts
+++ src/app/core/strava-service/strava.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { SummaryActivity } from '../../shared/models/strava/summaryactivity';
 
-import { firstValueFrom, Observable } from 'rxjs';
+import { catchError, first, firstValueFrom, Observable, throwError } from 'rxjs';
 import { CookieService } from '../cookie-service/cookie-service.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class StravaService {
 
   constructor(
     private httpClient: HttpClient,
-    private cookieService: CookieService) { }
+    private cookieService: CookieService,
+    private router: Router) { 
+    }
 
   _client_id = '49912';
   _client_secret = '84a5e674f6276b6da4d5a2a318624704e6c0546d';
@@ -24,13 +27,49 @@ export class StravaService {
     if (this.cookieService.check_exists('oauth_token')) {
       console.log('authenticate using cached token')
       this.oauth_token = this.cookieService.read('oauth_token')
+
+      // try to use oauth_token in a test request
+      if (this.get_authenticated_athlete()) {
+        const navigationExtras: NavigationExtras = {
+          state: {
+            access_token: this.oauth_token,
+            example: 'this is an example'
+          }
+        };
+        this.router.navigate(['map'], navigationExtras);
+      } else {
+        console.log('cached auth hasnt worked')
+        window.location.href = this.buildOAuthUrl();
+      }
     } else {
       console.log('there is no cached oauth token')
+      window.location.href = this.buildOAuthUrl();
     }
   }
 
   cache_access_token(token: string) {
     this.cookieService.create('oauth_token',token);
+  }
+
+  async get_authenticated_athlete(){
+    const requestOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + this.oauth_token
+      })
+    }
+    const get = this.httpClient
+      .get('https://www.strava.com/api/v3/athlete', requestOptions)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          // simple logging, but you can do a lot more, see below
+          console.log('An error occurred:', err.error);
+          return throwError(() => err);
+        })
+      );
+    return get.subscribe({
+      next: () => {return true},
+      error: () => {}
+    });
   }
 
   async getAccessToken(code: string){
@@ -76,5 +115,38 @@ export class StravaService {
     const get$ = this.httpClient.get<SummaryActivity[]>(url, requestOptions);
     
     return await firstValueFrom(get$)
+  }
+
+  buildOAuthUrl(): string {
+    const url_sep = [
+      'https:/',
+      'www.strava.com',
+      'oauth'
+    ];
+
+    let qp = ['authorize'];
+
+    // using redirect_base_uri here as window.location.href means the oauth flow will redirect back to the current base
+    // this would mean that launching the app from localhost or stackblitz would redirect accordingly
+    // however the strava api client is restricted to one authorisation callback domain
+    // either set this to localhost or gqailggqx.github.stackblitz.io (e.g.) to use either
+    let redirect_uri_components = [
+      window.location.href.slice(0, -1),
+      'exchange_token'
+    ];
+
+    const params_dict = {
+      client_id: 49912,
+      response_type: 'code',
+      redirect_uri: redirect_uri_components.join('/'),
+      approval_prompt: 'force',
+      scope: ['read', 'activity:read_all', 'activity:read'].join(',')
+    };
+
+    qp.push(Object.keys(params_dict).map(o=>o + '=' + params_dict[o]).join('&'))
+
+    url_sep.push(qp.join('?'))
+
+    return url_sep.join('/');
   }
 }
--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -9,51 +9,16 @@ import { StravaService } from '../../core/strava-service/strava.service';
 })
 export class LandingPageComponent implements OnInit {
 
+  public stravaOAuthUrlString: string;
+
   constructor(private stravaService: StravaService) {
   }
 
-  ngOnInit(): void {
-    this.setStravaOAuthUrlString();
+  ngOnInit(): void { }
+
+  startAuthFlow(){
+    // check for cached token
     this.stravaService.try_auth_from_cache();
-  }
 
-  public stravaOAuthUrlString: string;
-
-  setStravaOAuthUrlString() {
-    let redirect_base_uri = window.location.href.slice(0, -1);
-    this.stravaOAuthUrlString = this.BuildStravaOAuthUrl(redirect_base_uri);
-  }
-
-  BuildStravaOAuthUrl(redirect_base_uri: string): string {
-    const url_sep = [
-      'https:/',
-      'www.strava.com',
-      'oauth'
-    ];
-
-    let qp = ['authorize'];
-
-    // using redirect_base_uri here as window.location.href means the oauth flow will redirect back to the current base
-    // this would mean that launching the app from localhost or stackblitz would redirect accordingly
-    // however the strava api client is restricted to one authorisation callback domain
-    // either set this to localhost or gqailggqx.github.stackblitz.io (e.g.) to use either
-    let redirect_uri_components = [
-      redirect_base_uri,
-      'exchange_token'
-    ];
-
-    const params_dict = {
-      client_id: 49912,
-      response_type: 'code',
-      redirect_uri: redirect_uri_components.join('/'),
-      approval_prompt: 'force',
-      scope: ['read', 'activity:read_all', 'activity:read'].join(',')
-    };
-
-    qp.push(Object.keys(params_dict).map(o=>o + '=' + params_dict[o]).join('&'))
-
-    url_sep.push(qp.join('?'))
-
-    return url_sep.join('/');
   }
 }
