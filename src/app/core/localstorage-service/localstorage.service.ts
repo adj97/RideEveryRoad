9df0a74666ae3fc35db81c5f0a401cb9f45e4181
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-09-23 21:23:45 +0100

    encrypted activity data localstorage cache

--- src/app/core/localstorage-service/localstorage.service.ts
+++ src/app/core/localstorage-service/localstorage.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CryptoService } from '../crypto-service/crypto.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalstorageService {
 
-  constructor() { }
+  constructor(
+    private cryptoService: CryptoService
+  ) { }
 
   get(item_name: string){
-    return localStorage.getItem(item_name)
+    const raw_get = localStorage.getItem(item_name)
+    if(raw_get){
+      return this.cryptoService.decrypt(raw_get)
+    } else {
+      return raw_get
+    }
   }
 
   delete(item_name: string){
     return localStorage.removeItem(item_name)
   }
 
-  write(item_name: string, item: string){
-    return localStorage.setItem(item_name, item)
+  write(item_name: string, item: any){
+    const encrypted_item = this.cryptoService.encrypt(JSON.stringify(item))
+    return localStorage.setItem(item_name, encrypted_item)
   }
 }
