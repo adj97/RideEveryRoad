e7e34a31eac4f783891d244e52d7e4176e02ab42
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-09-25 00:30:44 +0100

    added keys from github secrets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 import { iEnvironment } from "./interfaces/environment";
-import { strava_app_client } from "./secrets/strava_app_client";
+
+const strava_app_client = {
+  id: '${process.env.STRAVA_API_CLIENT_ID}',
+  secret: '${process.env.STRAVA_API_CLIENT_SECRET}'
+}
 
 export const environment: iEnvironment = {
   production: true,
