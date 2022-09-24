b61eded89e26c9a6e9905097eeb9b06acc2b732e
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-09-25 00:46:38 +0100

    error in env variable injection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 import { iEnvironment } from "./interfaces/environment";
 
 const strava_app_client = {
-  id: '${process.env.STRAVA_API_CLIENT_ID}',
-  secret: '${process.env.STRAVA_API_CLIENT_SECRET}'
+  id: `${process.env.STRAVA_API_CLIENT_ID}`,
+  secret: `${process.env.STRAVA_API_CLIENT_SECRET}`
 }
 
 export const environment: iEnvironment = {
